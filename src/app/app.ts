8c78784f6a9419e4dbeee171808e325ac9bce9a3
KleberMera <klebermera2016@gmail.com>
2025-09-26 15:51:39 -0500

    config tailwind

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { InputOtpModule } from 'primeng/inputotp';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ InputOtpModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('applibertad');
-}
+  value: string = '';
+} 
